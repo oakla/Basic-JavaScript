c24b69e1d66521b8ea6560e36255e6ac56f3d0e1
Alex <>
2022-07-09 17:01:43 +1000

    add actual code

--- Roman Numeral Converter/code.js
+++ Roman Numeral Converter/code.js
@@ -1,3 +1,45 @@
-mod = 10
+function convertToRoman(num) {
 
-while(num)
+  // get #m's required
+  // add to string
+  
+  // repeat for each letter
+  
+    var rtn_string = ""
+  
+    let letters = {
+      'I': 1, 
+      'IV': 4, 
+      'V': 5, 
+      'IX': 9, 
+      "X": 10, 
+      'XL': 40, 
+      'L': 50, 
+      'XC': 90, 
+      'C': 100, 
+      'CD': 400, 
+      'D': 500, 
+      'CM': 900, 
+      'M': 1000
+    }
+  
+    let keys = ['I','IV','V','IX',"X",'XL','L','XC','C','CD','D','CM','M'].reverse()
+  
+    keys.forEach(function(letter) {
+      var x = letters[letter]
+      var quotient = Math.floor(num/x)
+  
+      num = num%x
+  
+      for(var i; i < quotient; i++) {
+        rtn_string += rn
+      }
+    })
+  
+  
+  
+  
+    return rtn_string;
+  }
+  
+  convertToRoman(36);
